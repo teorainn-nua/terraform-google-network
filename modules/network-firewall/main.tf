192c06afd81e9052bdf201bdb9bcc2c124a530be
Jag Gunawardana <jag.gunawardana@gmail.com>
2020-03-05 17:37:31 +0000

    Concat lists to fit args

--- modules/network-firewall/main.tf
+++ modules/network-firewall/main.tf
@@ -80,13 +80,12 @@ resource "google_compute_firewall" "private_allow_all_network_inbound" {
   target_tags = [local.private]
   direction   = "INGRESS"
 
-  source_ranges = [
+  source_ranges = concat([
     data.google_compute_subnetwork.public_subnetwork.ip_cidr_range,
     data.google_compute_subnetwork.public_subnetwork.secondary_ip_range[0].ip_cidr_range,
     data.google_compute_subnetwork.private_subnetwork.ip_cidr_range,
     data.google_compute_subnetwork.private_subnetwork.secondary_ip_range[0].ip_cidr_range,
-    data.google_compute_lb_ip_ranges.ranges.network,
-  ]
+  ], data.google_compute_lb_ip_ranges.ranges.network)
 
   priority = "1000"
 
